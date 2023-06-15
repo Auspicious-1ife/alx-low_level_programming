b9ec46b6d5e4c8b79adce8b16a6d3523ca7dd266
Auspicious-1ife <melissagushi50gmail.com>
2023-06-15 13:12:49 -0700

    Writing a script that prints "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+* main - This program writes a specific sentence to the console.
+* Return:0 (successful)
+*/
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
