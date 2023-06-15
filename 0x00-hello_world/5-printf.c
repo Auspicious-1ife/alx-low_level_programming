5556ef385fb06f91daac00e3eb8a34b842777b3c
Auspicious-1ife <melissagushi50gmail.com>
2023-06-15 13:31:24 -0700

    Writing  a script that prints  exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,7 @@
 int main(void)
 {
 /**
-* main - Entry point of the program.
-*
-* This function prints a specific sentence to the console.
-* The sentence showcases the importance of proper grammar.
-*
+* main - This program writes a specific sentence to the console.
 * Return:0 (on successful execution)
 */
 printf("with proper grammar, but the outcome is a piece of art,\n");
