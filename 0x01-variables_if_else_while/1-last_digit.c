fea0adde100032d47a0d3383a891dbf4bb984c9e
Auspicious-1ife <melissagushi50gmail.com>
2023-06-16 06:18:09 -0700

    Assigns a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,17 +12,19 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d ", n, abs(n % 10));
 if (n % 10 > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n");
+printf("and is greater than 5\n");
 }
-else if (n % 10 == 0)
+else if (n < 0)
 {
-printf("Last digit of %d is %d and is 0\n");
+printf("and is 0\n");
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n");
+printf("and is less than 6 and not 0\n");
 }
+printf("\n");
 return (0);
 }
