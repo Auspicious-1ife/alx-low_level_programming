0a7963f54ae06cd7ecf0ebfae323d33e57da0db6
Auspicious-1ife <melissagushi50@gmail.com>
2023-06-22 06:45:59 -0700

    Writing a function that draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_line - Prints a line of underscores in the terminal.
- * @n: The number of times the character '_' should be printed.
+ * print_line - draws a straight line in the terminal.
+ * @n: number of times the character _ should be printed.
  */
 void print_line(int n)
 {
-if (n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
-	} else
+	}
+	else
 	{
 		int i;
 
