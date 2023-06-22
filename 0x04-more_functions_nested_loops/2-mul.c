914b17d56c6e3970b8d70080c33501131f49c951
Auspicious-1ife <melissagushi50@gmail.com>
2023-06-22 03:21:58 -0700

    Writing a function that Write a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * multiply - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The product of the two integers.
+ */
+int multiply(int a, int b)
+{
+int product = a * b;
+return (product);
+}
