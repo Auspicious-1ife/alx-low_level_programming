89d114400205e09b9da2e56f4fda68fa0362b0fd
Auspicious-1ife <melissagushi50@gmail.com>
2023-07-03 16:39:20 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,18 +6,21 @@
  * @accept: Pointer to the string containing the bytes to search for.
  *
  * Return: Pointer to the byte in s that matches one of the bytes in accept,
- * and NULL if no such byte is found.
+ * or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-return (&s[i]);
-}
-}	
-return (NULL);
+    char *p;
+
+    while (*s != '\0')
+    {
+        for (p = accept; *p != '\0'; p++)
+        {
+            if (*s == *p)
+                return s;
+        }
+        s++;
+    }
+
+    return NULL;
 }
