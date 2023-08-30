76551b1da14075ac61492226c1befbd28ba87aa5
Auspicious-1ife <melissagushi50@gmail.com>
2023-08-30 11:26:21 -0700

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * str: input
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+char str[] = "Hello, programming world!\n";
+_puts_recursion(str);
+return (0);
+}
