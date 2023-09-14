2492bf438bcbc7be0385cb6c76369687dd6f1421
Auspicious-1ife <melissagushi50@gmail.com>
2023-09-13 17:46:32 -0700

    this is a program that performs 5 simple operations such as +, -, /, * and %, and prints the results in a new line

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+/**
+ * main - Entry point of the calculator program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Description:
+ * - Validates the number of command-line arguments.
+ * - Converts the input numbers and operator,
+ *   from strings to integers and characters.
+ * - Checks if the operator is valid and handles division by zero cases.
+ * - Calls the appropriate operation function based on the operator.
+ * - Prints the result of the operation and returns an exit status.
+ *
+ * Return: 0 on success, or an error code (98, 99, or 100) on failure.
+ */
 int main(int argc, char *argv[])
 {
 int digit1, digit2, result;
