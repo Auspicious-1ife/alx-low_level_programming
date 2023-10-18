63157e255957a34bf6ff658f9f3778582d48e79d
Auspicious-1ife <melissagushi50@gmail.com>
2023-10-18 11:01:40 -0700

    updated.

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * display_elf_header - Display information from the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
+void display_elf_header(Elf64_Ehdr *header) {
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < 16; i++) {
+        printf("%02x ", header->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" : "Unknown");
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %s\n", (header->e_type == ET_EXEC) ? "EXEC (Executable file)" : "Unknown");
+    printf("  Entry point address:               0x%lx\n", header->e_entry);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 98;
+    }
+
+    Elf64_Ehdr header;
+    if (read(fd, &header, sizeof(header)) != sizeof(header) {
+        perror("Error reading ELF header");
+        close(fd);
+        return 98;
+    }
+
+    if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Error: Not an ELF file\n");
+        close(fd);
+        return 98;
+    }
+
+    display_elf_header(&header);
+
+    close(fd);
+    return (0);
+}
