927485e6c34c82c77ac764057cfba2f50e2d801d
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-30 17:37:20 +0100

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,16 +9,15 @@
  */
 void print_array(int *array, size_t low, size_t high)
 {
-    size_t i;
-
-    printf("Searching in array: ");
-    for (i = low; i <= high; i++)
-    {
-        printf("%d", array[i]);
-        if (i < high)
-            printf(", ");
-    }
-    printf("\n");
+size_t i;
+printf("Searching in array: ");
+for (i = low; i <= high; i++)
+{
+printf("%d", array[i]);
+if (i < high)
+printf(", ");
+}
+printf("\n");
 }
 
 /**
@@ -33,26 +32,21 @@ void print_array(int *array, size_t low, size_t high)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t low, high, mid;
-
-    if (array == NULL)
-        return (-1);
-
-    low = 0;
-    high = size - 1;
-
-    while (low <= high)
-    {
-        print_array(array, low, high);
-        mid = (low + high) / 2;
-
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-
-    return (-1);
+size_t low, high, mid;
+if (array == NULL)
+return (-1);
+low = 0;
+high = size - 1;
+while (low <= high)
+{
+print_array(array, low, high);
+mid = (low + high) / 2;
+if (array[mid] == value)
+return (mid);
+else if (array[mid] < value)
+low = mid + 1;
+else
+high = mid - 1;
+}
+return (-1);
 }
